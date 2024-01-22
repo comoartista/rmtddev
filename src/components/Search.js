e09c9bb6505aa9443dd4b56c044ec9155aa6be07
Oksana Melnyk <oks.melnuk@gmail.com>
2024-01-22 09:36:33 +0100

    refactoring renderJobDetails

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,6 +8,7 @@ import {
 
 import renderError from './Error.js';
 import renderSpinner from './Spinner.js';
+import renderJobList from './JobList.js';
 
 const submitHandler = event => {
     event.preventDefault();
@@ -45,32 +46,11 @@ const submitHandler = event => {
             const { jobItems } = data;
 
             renderSpinner('search');
-            numberEl.textContent = jobItems.length;
-            
-            jobItems.slice(0, 7).map((jobItem) => {
-                const newJobItemHTML = `
-                    <li class="job-item">
-                        <a class="job-item__link" href="${jobItem.id}">
-                            <div class="job-item__badge">${jobItem.badgeLetters}</div>
-                            <div class="job-item__middle">
-                                <h3 class="third-heading">${jobItem.title}</h3>
-                                <p class="job-item__company">${jobItem.company}</p>
-                                <div class="job-item__extras">
-                                    <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i>${jobItem.duration}</p>
-                                    <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i>${jobItem.salary}</p>
-                                    <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i>${jobItem.location}</p>
-                                </div>
-                            </div>
-                            <div class="job-item__right">
-                                <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
-                                <time class="job-item__time">${jobItem.daysAgo}d</time>
-                            </div>
-                        </a>
-                    </li>
-                `
-                jobListSearchEl.insertAdjacentHTML("beforeend", newJobItemHTML);
-            })
 
+            numberEl.textContent = jobItems.length;
+
+            renderJobList(jobItems);
+            
             jobListSearchEl.addEventListener('click', (e) => {
                 e.preventDefault();
             })  
