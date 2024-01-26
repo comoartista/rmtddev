a663df2d556d6c73a46f3a3cce544626764d7094
Oksana Melnyk <oks.melnuk@gmail.com>
2024-01-26 12:48:37 +0100

    router component

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -53,6 +53,7 @@ const clickHandler = async event => {
     renderSpinner('job-details');
 
     const id = jobItemEl.children[0].getAttribute('href');
+    history.pushState(null, '', `/#${id}`);
 
     try { 
         const data = await getData(`${BASE_API_URL}/jobs/${id}`);
---
+++ src/components/Router.js
@@ -0,0 +1,37 @@
+import {
+    BASE_API_URL,
+    jobDetailsContentEl,
+    getData
+} from '../common.js';
+import renderSpinner from './Spinner.js';
+import renderJobDetails from './JobDetails.js';
+import renderError from './Error.js';
+
+const loadHashChangeHandler = async () => {
+    const id = window.location.hash.substring(1);
+
+    if(id) {
+        jobDetailsContentEl.innerHTML = '';
+
+        renderSpinner('job-details');
+
+        try { 
+            const data = await getData(`${BASE_API_URL}/jobs/${id}`);
+    
+            //extract job item
+            const { jobItem } = data;
+            
+            renderSpinner('job-details');
+    
+            renderJobDetails(jobItem);
+    
+        } catch {
+            renderSpinner('job-details');
+            renderError(error.message);
+        }
+    }
+
+}
+
+window.addEventListener('DOMContentLoaded', loadHashChangeHandler);
+window.addEventListener('hashchange', loadHashChangeHandler);
